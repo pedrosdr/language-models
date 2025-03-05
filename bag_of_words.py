c14ebe1077a4e401ea16eef28107e7b3c6aaa666
Pedro Sartori <pedrosartdr@gmail.com>
2025-03-05 00:15:09 -0300

    feat: Creates a model that predicts the labels based on a document-term matrix generated for a list of documents

---
+++ bag_of_words.py
@@ -0,0 +1,117 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import numpy as np
+import pandas as pd
+
+
+# Creating the corpus
+corpus = np.array([
+    "Movies are fun for everyone",
+    "Watching movies is great fun",
+    "Enjoy a great movie today",
+    "Research is interesting and important",
+    "Learning math is very important",
+    "Science discovery is interesting",
+    "Rock is great to listen to",
+    "Listen to music for fun",
+    "Music is fun for everyone",
+    "Listen to folk music"
+])
+
+# Extracting the vocabulary
+vocabulary = np.array(
+    " ".join([doc.strip() for doc in corpus]).lower().split(" ")
+)
+vocabulary = np.unique(vocabulary)
+vocabulary = np.sort(vocabulary)
+
+# Creating the document-term matrix
+lst = []
+for doc in corpus:
+    doc = doc.lower()
+    
+    lsti = []
+    for token in vocabulary:
+        if token in doc.split(" "):
+            lsti.append(1.0)
+        else:
+            lsti.append(0.0)
+    
+    lst.append(lsti)
+    
+dtm = torch.tensor(lst, dtype=torch.float32)
+print(dtm)
+
+# Labeling the documents (0: cinema, 1: science, 2: music)
+df = pd.DataFrame({
+    "doc": corpus,
+    "class": [0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
+})
+
+# Creating the matrices x and y
+x = dtm
+
+# Matrix y must be a dummy (one hot encoded)
+y = torch.tensor(
+    pd.get_dummies(df["class"], dtype="float32").to_numpy(), 
+    dtype=torch.float32
+)
+
+
+# Creating the model
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
+        self.linear1 = nn.Linear(x.size()[1], 100)
+        self.linear2 = nn.Linear(100, 100)
+        self.linear3 = nn.Linear(100, y.size()[1])
+        
+    def forward(self, x):
+        x = self.linear1(x)
+        x = f.relu(x)
+        x = f.dropout(x, 0.2)
+        
+        x = self.linear2(x)
+        x = f.relu(x)
+        x = f.dropout(x, 0.2)
+        
+        x = self.linear3(x)
+        x = f.softmax(x, dim=1)
+        return x
+    
+    
+# Instantiating the model, optimizer and loss criterion
+model = Model()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss() 
+
+# Training the model
+for i in range(100):
+    
+    ypred = model(x)
+    loss = criterion(y, ypred)
+    
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    print(f"epoch: {i}, loss: {loss.item()}")
+
+# Evaluating
+ypred = model(x)
+print(y.argmax(dim=1))
+print(ypred.argmax(dim=1))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
